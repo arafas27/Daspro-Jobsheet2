06cce176888720ac95d6c7fdaed68b1f234d9591
arafas27 <kiritodd10@gmail.com>
2024-09-09 20:15:08 +0700

    tambah file ke devel

---
+++ biodata02.java
@@ -0,0 +1,9 @@
+
+public class biodata02 {
+    public static void main(String[] args) {
+        System.out.println("NAMA :");
+        System.out.println("TTL");
+        System.out.println("ALAMAT");
+        System.out.println("HOBBY");
+    }
+}
